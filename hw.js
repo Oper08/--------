7928044da5d5e5c1c0869f90150eccdacebc3660
Ануар <seisenbaianuarr@gmail.com>
2024-11-21 09:00:50 +0500

    21.11.24 hw.js

--- hw.js
+++ hw.js
@@ -201,3 +201,43 @@ function bibka() {
 }
 
 bibka();
+/* 21.11.24
+function convert() { 
+    let euro = 0; 
+ 
+     
+ 
+    while (true) { 
+        
+        let summa = prompt("Соманы енгізіңіз (немесе 'стоп' жазыңыз):"); 
+        if (summa.toLowerCase() === "stop") 
+             break; 
+ 
+         
+        let valyuta = prompt("Валютаны енгізіңіз (USD, KZT, EUR):"); 
+        let valyuta1; 
+ 
+        if (valyuta === "USD") { 
+            valyuta1 = 0.85; 
+        } else if (valyuta === "KZT") { 
+           valyuta1 = 0.002; 
+        } else if (valyuta === "EUR") { 
+            valyuta1 = 1; 
+        } else { 
+           alert("Қате: дұрыс валютаны таңдаңыз (USD, KZT, EUR)!"); 
+            continue; 
+        } 
+ 
+         
+        let kobeitu = summa * valyuta1; 
+        euro += kobeitu; 
+ 
+        alert(Енгізілген сома: ${summa} ${valyuta}. Евроға аударылды: ${kobeitu} EUR.); 
+    } 
+alert(Барлық соманың евродағы қосындысы: ${euro} EUR.); 
+} 
+ 
+ 
+convert();
+*/
+
